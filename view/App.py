610869e95a6902cecc6b5688a0d66821c9db7eba
Lucas Augusto Hartmann <78509840+lucashartmann@users.noreply.github.com>
2025-08-24 18:04:56 -0300

    Add files via upload

---
+++ view/App.py
@@ -0,0 +1,24 @@
+from textual.app import App
+from view import TelaInicial, TelaEstoqueCapas
+from view.TelasLeitor import TelaLeitor
+from view.TelasAdmin import TelaAdmin
+from textual.binding import Binding
+
+class App(App):
+
+    
+    BINDINGS = {
+        Binding("s", "app.push_screen('tela_estoque')", "Tela Estoque")
+    }
+
+
+    SCREENS = {
+        "tela_inicial": TelaInicial.TelaInicial,
+        "tela_admin": TelaAdmin.TelaAdmin,
+        "tela_leitor": TelaLeitor.TelaLeitor,
+        "tela_estoque": TelaEstoqueCapas.TelaEstoqueCapas
+    }
+
+    def on_mount(self):
+        self.title = "Biblioteca"
+        self.push_screen("tela_inicial")
